bf32ff1f137c1ff119e71ce1c2c85b7cc7b50c07
DrMartiner <DrMartiner@GMail.Com>
2021-03-21 17:02:35 +0200

    Fixed serializers

--- apps/api/link/serializers.py
+++ apps/api/link/serializers.py
@@ -2,30 +2,33 @@ from rest_framework import serializers
 
 from apps.link import models
 
-__all__ = ["LinkCreateSerializer", "LinkListSerializer", "LinkRetrieveSerializer", "LinkUpdateSerializer"]
+__all__ = ["LinkCreateSerializer", "LinkListSerializer", "LinkRetrieveSerializer"]
 
 
 class LinkCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Link
-        fields = ["id", "link", "uses", "shortened", "created"]
-        read_only_fields = ["id", "uses", "shortened", "created"]
+        fields = "__all__"
 
 
 class LinkListSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Link
-        fields = ["id", "link", "uses", "shortened", "created"]
+        fields = "__all__"
+
+
+class VisitRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Visit
+        fields = ["visitor_data", "real_ip", "created"]
 
 
 class LinkRetrieveSerializer(serializers.ModelSerializer):
+    visits = serializers.SerializerMethodField(source="get_visits")
+
+    def get_visits(self, obj: models.Link) -> list[dict]:
+        return VisitRetrieveSerializer(obj.visit_set.all(), many=True).data
+
     class Meta:
         model = models.Link
-        fields = ["id", "link", "uses", "shortened", "created"]
-
-
-class LinkUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Link
-        fields = ["id", "link", "uses", "shortened", "created"]
-        read_only_fields = ["id", "uses", "shortened", "created"]
+        fields = ["uuid", "original_link", "visited_count", "created", "visits"]
