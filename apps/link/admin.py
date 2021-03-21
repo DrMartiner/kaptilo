eb9fc5426fe0c598108090825494fb5e1571bf99
DrMartiner <DrMartiner@GMail.Com>
2021-03-21 12:40:11 +0200

    Refactored links models

--- apps/link/admin.py
+++ apps/link/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
-from .models import *
+from . import models
 
 
-@admin.register(Link)
+class VisitInlineAdmin(admin.StackedInline):
+    model = models.Visit
+    extra = 0
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(models.Link)
 class LinkAdmin(admin.ModelAdmin):
-    list_display = ["shortened", "uses", "user", "created"]
+    inlines = [VisitInlineAdmin]
+    list_display = ["uuid", "visited_count", "created"]
     list_filter = ["created"]
-    search_fields = ["shortened", "link"]
+    search_fields = ["uuid", "original_link"]
+
+    def visited_count(self, obj: models.Link) -> int:
+        return models.Visit.objects.filter(link=obj).count()
+
+
+@admin.register(models.Visit)
+class VisitAdmin(admin.ModelAdmin):
+    list_display = ["id", "link", "count", "created"]
+    list_filter = ["created"]
+    search_fields = ["link__uuid", "link__original_link"]
--- apps/link/models.py
+++ apps/link/models.py
@@ -1,35 +1,44 @@
-import urllib
+import random
+import string
 
-import requests
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext as _
 
 __all__ = ["Link"]
 
 
+def get_short_link_uuid() -> str:
+    return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(8))
+
+
 class Link(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    link = models.URLField("Link", max_length=1024)
-    uses = models.IntegerField("Uses count", default=0)
-    shortened = models.URLField("Shortened link", null=True, default=None)
+    uuid = models.CharField(_("Uniq ID"), max_length=8, primary_key=True, default=get_short_link_uuid)
+    user = models.ForeignKey(User, models.CASCADE, verbose_name=_("User"))
+    original_link = models.URLField(_("Link"), max_length=1024)
+
     created = models.DateTimeField("Creation date", auto_now_add=True)
+
+    def __str__(self):
+        return f"Link {self.short_url} of UserID={self.user.pk}"
 
     class Meta:
         app_label = "link"
-        verbose_name = 'Link'
-        verbose_name_plural = 'Links'
+        ordering = ["user", "created"]
 
-    def get_shortened_link(self, request):
-        url = "http://" + request.get_host() + "/" + str(self.pk)
-        url = urllib.parse.quote(url)
-        r = requests.get('http://cutt.ly/api/api.php?key={}&short={}'.format(settings.CUTTLY_API_KEY, url))
-        data = r.json()
-        if data['url']['status'] == 7:
-            self.shortened = data['url']['shortLink']
-            self.save()
-            return True
-        return False
+    @property
+    def short_url(self) -> str:
+        return f"{settings.BASE_URL}{self.uuid}"
 
-    def __str__(self):
-        return "{} {}".format(self.user, self.pk)
+
+class Visit(models.Model):
+    link = models.ForeignKey("Link", models.CASCADE)
+    visitor_data = models.JSONField(_("Visitor data"), default=dict, null=True, blank=True)
+    count = models.IntegerField(_("Visited count"), default=0)
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        app_label = "link"
+        ordering = ["created"]
